5023c56d469038b0b6d5c7739f34dbe5e395377c
Jacob <jegalajda13@knights.ucf.edu>
2020-11-05 15:00:43 -0500

    Trying something again!

---
+++ server.js
@@ -0,0 +1,50 @@
+// server.js
+
+// import packages and express router
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+//const router = require('./routes/index');
+
+// call express to create running app object
+const app = express();
+
+// set port and mongoDB url (local or global)
+const PORT = 3001;
+//const MONGODB_URI = "mongodb://localhost:27017/my_local_db";
+//const MONGODB_URI = "mongodb+srv://root:!cop4331!@project.m58al.mongodb.net/Test?retryWrites=true&w=majority";
+const MONGODB_URI = "mongodb+srv://root:!cop4331!@project.m58al.mongodb.net/cop4331?retryWrites=true&w=majority";
+
+// ES6 Promises  -- set global Promise ES6 object equal to Promise
+// gets rid of DepreciatedWarning for Promises when running test
+mongoose.Promise = global.Promise;
+
+// connect to mongoDB with log messages for successful/unsuccessful connection
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false });
+mongoose.connection.once('open', function() {
+    console.log('Connected to the Database.');
+});
+mongoose.connection.on('error', function(error) {
+    console.log('Mongoose Connection Error : ' + error);
+});
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api', require('./routes/api'))
+
+// Check production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'))
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+// listen for connects to app at the port listed above
+app.listen(PORT, function() {
+    console.log(`Server listening on port ${PORT}.`);
+});
