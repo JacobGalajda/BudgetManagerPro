56ec2668fa793ec565c5bb5a096c3d47e6c18bed
louischamp <luizgustavorocco@hotmail.com>
2020-11-17 16:52:59 -0800

    More work on authentication api

--- routes/auth.js
+++ routes/auth.js
@@ -10,8 +10,19 @@ const Users = require('../models/users');
 // API endpoint to login and generate authentication
 
 router.post('/login', function(req, res) {
-    console.log(req.body);
-    const {email, password} = req.body
+    const {email, password} = req.body.userData; // IFFY ON THIS LINE, I don't know what user data is
 
-    Users.findOne();
+    if(email === undefined || password === undefined) {
+        res.status(401).json({
+            success: false,
+            message: "Email and/or password are invalid."
+        })
+    } else {
+        Users.findOne({email: email , password: password}).then(function(err, user) {
+            if(!err && result !== null) {
+                
+            }
+        });
+    }
+    
 });
