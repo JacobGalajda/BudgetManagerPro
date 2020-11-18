06ca7a397a5f0abd1f520bc7568860d20413cd74
louischamp <luizgustavorocco@hotmail.com>
2020-11-18 00:03:09 -0800

    auth/login api endpoint tested in postman generates correct authentication token

--- routes/auth.js
+++ routes/auth.js
@@ -11,34 +11,27 @@ const Users = require('../models/users');
 // API endpoint to login and generate authentication
 
 router.post('/login', function(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    if(email === undefined || password === undefined) {
-        res.status(401).json({
-            success: false,
-            message: "Email and/or password are invalid."
-        })
-    } else {
-        Users.findOne({email: email , password: password}).then(function(err, result) {
-            if(!err && result !== null) {
-                let tokenData = {
-                    id: result.id,
-                    name: result.name,
-                    email: result.email
-                }
-                let generatedToken = jwt.sign(tokenData, config.JWT_KEY, {expiresIn: '30m'});
-                res.json({
-                    token: generatedToken
-                });
-            } else {
-                res.status(401).json({
-                    success: false,
-                    message: err || 'User does not exist.'
-                })
+    console.log(req.body);
+    //const {email, password} = req.body;
+    Users.findOne(req.body, function(err, result) {
+        if(!err && result !== null) {
+            let tokenData = {
+                id: result.id,
+                name: result.name,
+                email: result.email
             }
-        });
-    }
+            let generatedToken = jwt.sign(tokenData, config.JWT_KEY, {expiresIn: '30m'});
+            res.json({
+                token: generatedToken
+            });
+        } else {
+            res.status(401).json({
+                success: false,
+                message: err || 'User does not exist.'
+            })
+        }
+    });
+
     
 });
 
