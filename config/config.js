11d477aafe08ef71e40d688c4c59699d48d3088e
louischamp <luizgustavorocco@hotmail.com>
2020-11-17 22:31:52 -0800

    Added config folder and file and finished the login api that authenticates an user by returning a jwt token

---
+++ config/config.js
@@ -0,0 +1,6 @@
+const config = {}
+
+config.JWT_KEY = 'supersecretkeythatweneedtochangeinthegfuture';
+config.MONGO_URL = 'mongodb+srv://root:!cop4331!@project.m58al.mongodb.net/cop4331?retryWrites=true&w=majority';
+
+module.exports = config;
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const config = require('../config');
 
 const jwt = require('jsonwebtoken');
 
@@ -18,11 +19,26 @@ router.post('/login', function(req, res) {
             message: "Email and/or password are invalid."
         })
     } else {
-        Users.findOne({email: email , password: password}).then(function(err, user) {
+        Users.findOne({email: email , password: password}).then(function(err, result) {
             if(!err && result !== null) {
-                
+                let tokenData = {
+                    id: result.id,
+                    name: result.name,
+                    email: result.email
+                }
+                let generatedToken = jwt.sign(tokenData, config.JWT_KEY, {expiresIn: '30m'});
+                res.json({
+                    token: generatedToken
+                });
+            } else {
+                res.status(401).json({
+                    success: false,
+                    message: err || 'User does not exist.'
+                })
             }
         });
     }
     
 });
+
+module.exports = router;
