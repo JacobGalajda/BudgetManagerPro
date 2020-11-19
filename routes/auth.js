21673b19b5b3916938fa854bccf105c4d4d476c5
louischamp <luizgustavorocco@hotmail.com>
2020-11-18 16:22:04 -0800

    auth.js example for David

--- routes/auth.js
+++ routes/auth.js
@@ -7,32 +7,26 @@ const jwt = require('jsonwebtoken');
 //var bcrypt = require('bcrypt'); encryption library if needed in the future
 
 const Users = require('../models/users');
+const users = require('../models/users');
 
 // API endpoint to login and generate authentication
 
 router.post('/login', function(req, res, next) {
-    console.log(req.body);
-    console.log(req.body.email);
-    Users.find(req.body).then(function(user) {
-        if(res !== null) {
-            let tokenData = {
-                id: res.id,
-                name: res.name,
-                email: res.email
-            }
-            let generatedToken = jwt.sign(tokenData, config.JWT_KEY, {expiresIn: '30m'});
-            res.send({
-                token: generatedToken,
-                id: res.id,
-                name: res.name
-            });
-        } else {
-            res.status(401).send({
-                success: false,
-                message: 'User does not exist.'
-            })
-        }
-    }).catch(next);  
+    console.log(req.body); // REMOVE LATER TESTING
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if(email === undefined || password === undefined) {
+        res.status(401).json({
+            success: false,
+            message: "Email and/or password are invalid."
+        });
+    } else {
+        Users.find(req.body).then(function(user) {
+            console.log(user.id); //THIS ALWAYS GIVES ME UNDEFINED
+            res.send(user);
+        }).catch(next);
+    } 
 });
 
 module.exports = router;
