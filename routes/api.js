7933f3b93b758215b286b6cdcd238fb33550af99
louischamp <luizgustavorocco@hotmail.com>
2020-12-02 11:10:43 -0800

    EMail verification test 1

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,24 @@
-// routes/index.js
-
+/ routes/index.js
+// require express
 const express = require('express');
+
+//require config variable
+const config = require('../config');
+
+//require sendgrid/mail
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(config.SECRET_API_KEY);
+
+//require crypto
+const crypto = require('crypto');
+
 
 // create instance of express router for interpreting routes
 const router = express.Router();
 
-// import Users database model
+// import Users/Budget database model
 const Users = require('../models/users');
+const Budget = require('../models/budget');
 
 
 
@@ -20,13 +32,66 @@ router.get('/users', function(req, res) {
 });
 
 // API endpoint - post new user
-router.post('/users', function(req, res, next) {
-    console.log(req.body)
-
-    
+router.post('/users', async function(req, res, next) {
     Users.create(req.body).then(function(user) {
-        res.send(user);
+        const emailToken = crypto.randomBytes(64).toString('hex');
+        const msg = {
+            to: req.body.email,
+            from: 'noreply@email.com',
+            subject: 'Budget Manager Pro - Verify your account',
+            text: `
+            Hello, thanks for registering on our website.
+            Please copy and paste the address below to verify your account.
+            http://${req.headers.host}/verify-email?token=${emailToken}
+            `,
+            html: `
+            <h1> Hello,</h1>
+            <p>Thanks for registering on our website,</p>
+            <p>Please click the link below to verify your account.</p>
+            <a href="http://${req.headers.host}/verify-email?token=${emailToken}"> Verify your account</a>
+            `
+        };
+        try {
+            await sgMail.send(msg);
+            res.send({
+                success: true,
+                message: 'Thank you for registering. Please check your email to verify your account.'             
+            });
+            //res.redirect('/');
+        } catch(error) {
+            console.log(error);
+            res.status(401).send({
+                success: false,
+                message: 'Something went wrong. Please contact us at noreply@email.com'
+            });
+        }
     }).catch(next);
+});
+
+// Email verification route
+router.get('/verify-email', async (req, res, next) => {
+    try {
+        const user = await Users.findOne({emailToken: req.query.token});
+        if (!user) {
+            res.status(401).send({
+                success: false,
+                message: "Token is invalid. Please contact us for assistance."
+            });
+        }
+        user.emailToken = null;
+        user.verified = true;
+        await user.save();
+        res.send({
+            success: true,
+            message: "User verified."
+        });
+    } catch(error) {
+        console.log(error);
+        res.status(401).send({
+            success: false,
+            message: "Verification failed. Please contact us for assistance."
+        });
+    }
 });
 
 // API endpoint - update user
@@ -42,7 +107,124 @@ router.put('/users/:id', function(req, res) {
 });
 
 // API endpoint - delete user by id
-//router.delete('/articles/:id', function(req, res) {})
+router.delete('/users/:id', function(req, res) {
+    //res.send({ type: 'DELETE' });
+
+    // find user document by id, delete
+    Users.findByIdAndRemove({ _id: req.params.id }, req.body).then(function(user) {
+        // check if this works by finding User's unique _id and checking for update
+        //Users.findOne({ _id: req.params.id }).then(function(user) {
+
+        // send update back to as response
+        res.send(user);
+        //});
+    });
+})
+
+// API endpoint - get list of all THIS users budgets
+router.get('/users/:id/budgets', function(req, res) {
+    // user_id?
+    //user_id = req.session.passport.user;
+    //res.send(req.params.id);
+
+    //
+    Users.findById(req.params.id, function(err, user) {
+        // check for errors, respond if occurs
+        if (err) {
+            res.send(err);
+        };
+
+        // print name of first budget to console
+        //console.log(user.user_budgets[0].budget_name);
+
+        // respond with array of user_budgets
+        res.json(user.user_budgets)
+
+    });
+
+});
+
+// API endpoint - post new budget
+router.post('/users/:id/budgets', function(req, res, next) {
+    //
+    console.log(req.body)
+
+    // create new budget schema from json obj sent in post request body
+    const newBudget = new Budget(req.body);
+
+    //
+    Users.findById(req.params.id, function(err, user) {
+        // check for errors, respond if occurs
+        if (err) {
+            res.send(err);
+        };
+
+        // add new budget to new user's budget array
+        user.user_budgets.push(newBudget);
+        res.json(user);
+
+    });
+
+});
+
+// API endpoint - update budget by user id
+router.put('/users/:id/budgets/:budget_id', function(req, res) {
+    // create a new user object based on Budget model
+    updatedBudget = new Budget(req.body);
+
+    //console.log(req.params.budget_id);
+
+    // find user document by name and update (luigi)
+    Users.findOneAndUpdate({ "_id": req.params.id }, { user_budgets: updatedBudget }).then(function(err, user) {
+        // check for errors, respond if occurs
+        if (err) {
+            res.send(err);
+        };
+
+        // return user object to verify change
+        res.json(user);
+    });
+});
+
+// API endpoint - delete budget by user id
+router.delete('/users/:id/budgets/:budget_id', function(req, res) {
+    // create a new user object based on Budget model
+    //updatedBudget = new Budget(req.body);
+
+    //console.log(req.params.budget_id);
+
+    //
+    Users.findById(req.params.id, function(err, user) {
+        // check for errors, respond if occurs
+        if (err) {
+            res.send(err);
+        };
+
+        // delete budget from user's budget array
+        user.user_budgets.pull(req.params.budget_id);
+        user.save();
+        res.json(user);
+
+    });
+});
+
+// API endpoint - get expenses
+// API endpoint - create expenses
+// API endpoint - update expenses
+// API endpoint - delete expenses
+
+
+// API endpoint - get income
+// API endpoint - create income
+// API endpoint - update income
+// API endpoint - delete income
+
+
+// API endpoint - get goals
+// API endpoint - create goals
+// API endpoint - update goals
+// API endpoint - delete goals
+
 
 // expoert router object with Article endpoint (collection in Test database)
 module.exports = router;
