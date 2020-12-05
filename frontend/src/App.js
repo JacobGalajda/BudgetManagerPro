a375a5101b7343be54b4015dcb87bb48e441fb6b
Jacob <jegalajda13@knights.ucf.edu>
2020-12-05 17:57:44 -0500

    Committing :D

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
         </Switch>
       <Switch>
         <Route exact path="/">
-          <Redirect to="/login"/>
+          <Redirect to="/login" />
         </Route>
       </Switch>
       </BrowserRouter>
