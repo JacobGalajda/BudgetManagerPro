2745abd31153575370dc77cb8e429feafcc017f6
Jacob <jegalajda13@knights.ucf.edu>
2020-12-05 16:21:27 -0500

    React-ified front end web

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,22 @@
 import './App.css';
 import './index.css'
 import './budgetFormatting.css'
-import React, {Component, useState} from 'react';
-
+import React, {Component, Fragment, useState} from 'react';
+import Login from './Login.js';
 import bootsrap from 'react-bootstrap';
+import SignUp from './SignUp';
+import ForgotPassword from './ForgotPassword';
 
 
 class App extends Component {
   render() {
-  return (
-    <div className="App-header">
-      <body className="center">
-        <br></br>
-        <br></br>
-        <h1>Budget Manager</h1>
-        <h1>Pro</h1>
-        <br></br>
-
-        <h6>A place to organize finances to ensure that they are within their financial scopes.</h6>
-        <div className="white-box">
-            <form className="box">
-              <p id="loginResult"></p>
-            
-              <input type="text" id="loginName" placeholder="email"></input>
-              <input type="password" id="loginPassword" placeholder="Password"></input>
-            
-              <a href>Forgot password...</a>
-              <br></br>
-              <a href>Sign up</a>
-
-              <button id="loginButton" onclick="">Login</button>
-            </form>
-        </div>
-
-        </body>
-      </div>
-  );
+    return (
+      <Fragment>
+        <Login/>
+        <SignUp/>
+        <ForgotPassword/>
+      </Fragment>
+    );
   }
 }
 
