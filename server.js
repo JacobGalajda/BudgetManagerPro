f0cd37d6fe2ad0bec0ba4188e1c6c1d5b2f93cc7
Jacob <jegalajda13@knights.ucf.edu>
2020-12-07 17:31:48 -0500

    Words

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use('/auth', require('./routes/auth'));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'))
 
-    app.get('*', function(req, res) {
+    app.get('/*', function(req, res) {
         res.sendFile(path.resolve(__dirname, 'frontend/build', 'index.html'));
     });
 }
