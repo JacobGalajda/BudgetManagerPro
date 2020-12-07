76fb12664bfdeef2527ec013fc305320a61e137e
Jacob <jegalajda13@knights.ucf.edu>
2020-12-07 17:14:10 -0500

    Please work

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'))
 
     app.get('*', function(req, res) {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'frontend/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend/build', 'index.html'));
     });
 }
 
